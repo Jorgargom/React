8fe60280a776d05c4e7691d7af1e4074132eb438
Jorge García Gómez <jorge@jorgegarciagomez.es>
2018-09-19 09:44:28 +0200

    creo app para gastosemanal

---
+++ gastosemanal/src/componentes/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+    
+    <header>
+        <h1>{props.titulo}</h1>
+    </header>
+    
+    )
+
+}
+
+
+export default Header; 
