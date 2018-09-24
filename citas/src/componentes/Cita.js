bf99dfb106d18d51e0bf1e29c872d6ea5de6cb22
@jorgegarcia17041981 <jorgegarcia17041981@gmail.com>
2018-09-24 16:31:23 +0200

    creando el componente para las citas

--- citas/src/componentes/Cita.js
+++ citas/src/componentes/Cita.js
@@ -3,8 +3,23 @@ import React, { Component } from 'react'
 class Cita extends Component {
     
     render() { 
+        const {fecha, hora, mascota, propietario, sintomas} = this.props.info;
         return ( 
-            <p>Desde cita</p>
+            <div className="media mt-3">
+                <div className="media-body">
+                    <h3 className="mt-0">{mascota}</h3>
+                    <p className="card-text"><span>Nombre Dueño: </span> {propietario}</p>
+                    <p className="card-text"><span>Fecha: </span>{fecha}</p>
+                    <p className="card-text"><span>Hora: </span>{hora}</p>
+                    <p className="card-text"><span>Sintomas: </span><br />
+                        {sintomas}
+                    </p>
+                    <button className="btn btn-danger">Borrar &times;</button>
+
+
+
+                </div>
+            </div>
          );
     }
 }
