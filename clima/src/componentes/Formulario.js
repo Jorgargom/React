a94fa2f0e81ab9f7b26832580bfd52c76036feca
@jorgegarcia17041981 <jorgegarcia17041981@gmail.com>
2018-09-25 11:12:42 +0200

    leyendo los datos del formulario

--- clima/src/componentes/Formulario.js
+++ clima/src/componentes/Formulario.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 
 class Formulario extends Component {
+
+    // Crear los refs 
+
+    ciudadRef = React.createRef();
+    paisRef = React.createRef();
+    
+    buscarClima = (e) => {
+        e.preventDefault();
+    
+    // Leer los refs y crear el objeto
+
+    const respuesta = {
+        ciudad: this.ciudadRef.current.value,
+        pais: this.this.paisRef.current.value
+    }
+
+    // Enviar los props
+
+
+
+    // opcional resetear el form
+
+
+
+
+    }
     
     render() { 
         return ( 
-            <div className="contendor-form">
+            <div className="contenedor-form">
                 <div className="container">
                     <div className="row">
-                        <form>
+                        <form onSubmit={this.buscarClima}>
                            <div className="input-field col s12 m8 l4 offset-2">
-                                <input id="ciudad" type="text" />
+                                <input id="ciudad" ref={this.ciudadRef} type="text" />
                                 <label htmlFor="ciudad">Ciudad:</label>
                            </div> 
                            <div className="input-field col s12 m8 l4 offset-2">
-                                <select>
+                                <select ref={this.paisRef}>
                                     <option value="" defaultValue>Eligue un pais</option>
                                     <option value="AR">Argentina</option>
                                     <option value="CO">Colombia</option>
