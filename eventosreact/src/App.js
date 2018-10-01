0cf6522de7b1ecc7cfa2d466eb8a0290b96c9824
@jorgegarcia17041981 <jorgegarcia17041981@gmail.com>
2018-10-01 22:45:07 +0200

    Guardo las categorias en el state

--- eventosreact/src/App.js
+++ eventosreact/src/App.js
@@ -3,19 +3,28 @@ import Header from './componentes/Header';
 
 class App extends Component {
 
+  token = '546LVYG4K4YAZ4SQNB3T';
+
+  state = {
+    categorias: []
+  }
+  
   componentDidMount(){
     this.obtenerCategorias();
   }
 
   obtenerCategorias = async () => {
-    let url = `https://www.eventbriteapi.com/v3/categories/?token=546LVYG4K4YAZ4SQNB3T&locale=es_ES`;
+    let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale=es_ES`;
 
     await fetch(url)
       .then(respuesta => {
         return respuesta.json();
       })
       .then(categorias => {
-        console.log(categorias);
+        this.setState({
+          categorias: categorias.categories
+        })
+        
       })
   }
 
