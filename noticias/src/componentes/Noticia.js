dbc8979a64e62e606ffec2ba58fd4179dc7e494a
@jorgegarcia17041981 <jorgegarcia17041981@gmail.com>
2018-10-01 15:48:32 +0200

    mostrando las noticias

--- noticias/src/componentes/Noticia.js
+++ noticias/src/componentes/Noticia.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
-const Noticia = () => {
+const Noticia = (props) => {
 
     const {urlToImage, url, title, description, source} = props.noticia;
 
     return (
-        <div>
-            <p>Desde Noticia</p>
+        <div className="col s12 m6 l4">
+            <div className="card">
+                <div className="card-image">
+                    <img src={urlToImage} alt={title} />
+                    <span className="card-title">{source.name}</span>
+                </div>
+                <div className="card-content">
+                    <h3>{title}</h3>                   
+                    <p>{description}</p>
+                </div>
+                <div className="card-action">
+                    <a href={url} target="_blank" className="waves-effects waves-light btn">
+                        Noticia Completa
+                    </a>                   
+                    <p>{description}</p>
+                </div>
+            </div>
         </div>
     )
 }
