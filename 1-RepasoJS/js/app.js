c7b0f129cf37f3079aa469138447a163c3b89cf0
Jorge García Gómez <jorge@jorgegarciagomez.es>
2019-07-18 22:15:14 +0200

    Update app.js

--- 1-RepasoJS/js/app.js
+++ 1-RepasoJS/js/app.js
@@ -83,3 +83,91 @@ const cliente = function (nombreCliente) {
 
 cliente('Jorge');
 
+// Parametros por default en las funciones
+
+function actividad(nombre = 'Javi', actividad = 'Aprendiendo Javascript') {
+    console.log(`La persona ${nombre}, esta realizando la actividad ${actividad}`);
+    
+}
+
+actividad('Jorge', 'Aprendiendo Javascript');
+actividad('Patri', 'Aprendiendo Javascript');
+actividad('Francisco');
+
+// Arrow Functions
+
+let viajando = (destino, duracion) => {
+    return `Viajando a la ciudad de: ${destino} durante ${duracion}`;
+}
+
+let viaje;
+viaje = viajando('Paris');
+viaje = viajando('Paris', '10 días');
+
+console.log(viaje);
+
+// Objetos
+
+
+// Object literal
+
+const persona1 = {
+    nombre: 'Jorge',
+    profesion: 'Web Developer',
+    edad: '38',
+}
+
+const persona2 = {
+    nombre: 'Jorge',
+    profesion: 'Web Developer',
+    edad: '38',
+}
+
+console.log(persona1);
+console.log(persona2);
+
+// Object constructor
+
+function Tarea (nombre, urgencia) {
+    this.nombre = nombre;
+    this.urgencia = urgencia;
+}
+
+const tarea1 = new Tarea ('Aprender Javascript', 'Urgente');
+const tarea2 = new Tarea ('Aprender PHP', 'Urgente');
+const tarea3 = new Tarea ('Aprender React', 'Urgente');
+const tarea4 = new Tarea ('Aprender Java', 'Urgente');
+
+console.log(tarea1);
+console.log(tarea2);
+console.log(tarea3);
+console.log(tarea4);
+
+
+// Prototypes
+
+function Cosas (nombre, urgencia) {
+    this.nombre = nombre;
+    this.urgencia = urgencia;
+}
+
+Cosas.prototype.mostrarInformacionCosas = function() {
+    return `La tarea ${this.nombre} tiene una prioridad de ${this.urgencia}`;
+}
+
+const cosas1 = new Cosas('Aprendiendo Javascript', 'Urgentísimo');
+const cosas2 = new Cosas('Aprendiendo React', 'Urgentísimo');
+
+
+console.log(cosas1.mostrarInformacionCosas());
+console.log(cosas2.mostrarInformacionCosas());
+
+
+// Destructuring de objetos
+
+const aprendiendoJavascript = {
+    version: 'ES6+',
+    frameworks: ['React', 'VueJS', 'AngularJS']
+}
+
+console.log(aprendiendoJavascript);
