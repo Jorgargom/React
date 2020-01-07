3cda2bb766ef0ae06ac3b55765418bd02a458579
Jorge García Gómez <jorge@jorgegarciagomez.es>
2020-01-07 20:14:20 +0100

    app basica

---
+++ 2020/basicosreact/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    React.createElement(
+      'h1',
+      {id:'heading', className: 'heading'},
+      'Hola Mundo'
+    )
+  );
+}
+
+<h1 class="heading" id="heading">Hola Mundo</h1>
+
+
+export default App;
