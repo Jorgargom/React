1e1a64f68005931dcc66c2a3d3daa5e87d7bcd76
Jorge García Gómez <jorge@jorgegarciagomez.es>
2020-09-01 19:27:31 +0200

    repaso js

--- 2020/curso_react_fh/02-intro-javascript/src/index.js
+++ 2020/curso_react_fh/02-intro-javascript/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Variables y Constantes
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const nombre = "Jorge";
+const apellidos = "García Gómez";
+
+let valorDado = 5;
+valorDado = 7;
+
+
+console.log(nombre, apellidos, valorDado);
+
+if (true) {
+  let valorDado = 6; 
+  console.log(valorDado);
+}
+
+console.log(valorDado);
