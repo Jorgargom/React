5ed074abb0943f25e16c6f1107c8445b014d6e8d
Jorge García Gómez <jorge@jorgegarciagomez.es>
2020-09-02 21:52:16 +0200

    const y let

---
+++ 2020/curso_react_fh/02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,19 @@
+
+// Variables y Constantes
+
+const nombre = 'Jorge';
+const apellido = '´García Gómez';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log( nombre, apellido, valorDado )
+
+// var No se debe de usar...
+if ( true ) {
+    const nombre = 'Peter';
+    console.log(nombre)
+}
+
+console.log( valorDado );
+
