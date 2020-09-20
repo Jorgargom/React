715a67b036a2a297c9694176a22a62295aabde84
Jorge García Gómez <jorge@jorgegarciagomez.es>
2020-09-20 21:50:05 +0200

    hoooks - useState

--- 2020/curso_react_fh/03-counter-app/src/CounterApp.js
+++ 2020/curso_react_fh/03-counter-app/src/CounterApp.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ( {value } ) => {
 
+    const [counter, setCounter] = useState(0); 
+
+
     const handleAdd = (e) => {
-        console.log(e);
+        setCounter(counter + 1);
     }
 
 
@@ -13,7 +16,7 @@ const CounterApp = ( {value } ) => {
       
         < >
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
 
             <button onClick={ handleAdd }>+1</button>
 
