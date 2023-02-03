68c2f2e7908c6ff605431ee3dbe1b10549c44c78
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-02-03 21:59:43 +0100

    Hello World

--- 2023/React-FH/02-counter-app-vite/src/main.jsx
+++ 2023/React-FH/02-counter-app-vite/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function App() {
+    //document.createElement...
+    return (<h1>Hola Mundo!!!</h1>)
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
