1dda18cfd227ef37abd2578720563b46c7b814d2
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-02-04 19:47:21 +0100

    variable printing in html

--- 2023/React-FH/02-counter-app-vite/src/FirstApp.jsx
+++ 2023/React-FH/02-counter-app-vite/src/FirstApp.jsx
@@ -1,7 +1,13 @@
+  
+const getResult = (a, b) => {
+    return a + b
+  }
+
 export const FirstApp = () => {
+  
   return (
     <>
-      <h1>Hello I´am George</h1>
+      <h1>Hello I´am { getResult(10, 5) }</h1>
       <p>I´am Fragment</p>
     </>
   )
