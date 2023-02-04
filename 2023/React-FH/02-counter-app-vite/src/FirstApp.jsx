a5d9092ab860800fbaca4d5e26b5daaa96e28947
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-02-04 19:34:28 +0100

    fragment

---
+++ 2023/React-FH/02-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,8 @@
+export const FirstApp = () => {
+  return (
+    <>
+      <h1>Hello I´am George</h1>
+      <p>I´am Fragment</p>
+    </>
+  )
+}
--- 2023/React-FH/02-counter-app-vite/src/main.jsx
+++ 2023/React-FH/02-counter-app-vite/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelloWorldApp } from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <FirstApp />
         <HelloWorldApp />
     </React.StrictMode>
 );
