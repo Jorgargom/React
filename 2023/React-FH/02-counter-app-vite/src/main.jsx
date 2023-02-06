72aad2b4f6ac60664f14521d6ad601b253e0b645
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-02-06 21:20:58 +0100

    place CSS styles

--- 2023/React-FH/02-counter-app-vite/src/main.jsx
+++ 2023/React-FH/02-counter-app-vite/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelloWorldApp } from './HelloWorldApp';
 import { FirstApp } from './FirstApp';
-
+import './styles.css';
 
 
 
