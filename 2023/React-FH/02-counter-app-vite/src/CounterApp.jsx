df991bafd8a9bacd8b35042c59deed542c9e1990
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-02-08 11:16:34 +0100

    useState

--- 2023/React-FH/02-counter-app-vite/src/CounterApp.jsx
+++ 2023/React-FH/02-counter-app-vite/src/CounterApp.jsx
@@ -1,18 +1,23 @@
-import PropTypes from 'prop-types'
-;
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 export const CounterApp = ({value}) => {
   
+    const [ counter, setCounter ] = useState( 10 );
+
+
+
     const handleAdd = (event) => {
-        console.log(value)
-        value = 1000;
+
+        setCounter(counter + 1);
+        // setCounter( ( c )=> c + 1 )
     }
   
   
     return (
             <>
             <h1>CounterApp</h1>
-            <h2>{ value }</h2>
+            <h2>{ counter }</h2>
             <button onClick={ handleAdd }>
                     +1
             </button>
