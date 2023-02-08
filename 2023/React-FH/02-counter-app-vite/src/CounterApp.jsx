59c7ce2796ae65c45a21c7b4bffa838ba6104ed9
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-02-08 11:05:08 +0100

    event click

--- 2023/React-FH/02-counter-app-vite/src/CounterApp.jsx
+++ 2023/React-FH/02-counter-app-vite/src/CounterApp.jsx
@@ -2,10 +2,20 @@ import PropTypes from 'prop-types'
 ;
 
 export const CounterApp = ({value}) => {
-  return (
+  
+    const handleAdd = (event) => {
+        console.log(value)
+        value = 1000;
+    }
+  
+  
+    return (
             <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2>{ value }</h2>
+            <button onClick={ handleAdd }>
+                    +1
+            </button>
             </>
       )
 }
