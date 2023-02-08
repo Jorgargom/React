58ce87b674036b902a80cded2c62dd2765a585ee
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-02-08 10:52:38 +0100

    CounterApp

--- 2023/React-FH/02-counter-app-vite/src/main.jsx
+++ 2023/React-FH/02-counter-app-vite/src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import { HelloWorldApp } from './HelloWorldApp';
-import { FirstApp } from './FirstApp';
+//import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
+
 import './styles.css';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp title="Title" subTitle={123}/>
+        <CounterApp value={10}/>
         {/* <HelloWorldApp /> */}
     </React.StrictMode>
 );
