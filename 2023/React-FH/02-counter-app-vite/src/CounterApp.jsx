bed18b0628d8b95e5989f44853d4a43f43e6ba95
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-02-09 21:48:25 +0100

    handleSubtract y handleReset

--- 2023/React-FH/02-counter-app-vite/src/CounterApp.jsx
+++ 2023/React-FH/02-counter-app-vite/src/CounterApp.jsx
@@ -1,32 +1,31 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export const CounterApp = ({value}) => {
-  
-    const [ counter, setCounter ] = useState( 10 );
+export const CounterApp = ({ value }) => {
+    
+    const [ counter, setCounter ] = useState( value );
 
-
-
-    const handleAdd = (event) => {
-
-        setCounter(counter + 1);
-        // setCounter( ( c )=> c + 1 )
+    const handleAdd = () => { 
+        // console.log(event)
+        setCounter( counter + 1 );
+        // setCounter( (c) => c + 1 )
     }
-  
-  
-    return (
-            <>
-            <h1>CounterApp</h1>
-            <h2>{ counter }</h2>
-            <button onClick={ handleAdd }>
-                    +1
-            </button>
-            </>
-      )
-}
 
+    const handleSubstract = () => setCounter( counter - 1 );
+    const handleReset = () => setCounter( value );
+
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2> { counter } </h2>
+
+            <button onClick={ handleAdd }> +1 </button>
+            <button onClick={ handleSubstract }> -1 </button>
+            <button aria-label="btn-reset" onClick={ handleReset }> Reset </button>
+        </>
+    );
+}
 
 CounterApp.propTypes = {
     value: PropTypes.number.isRequired
-  }
-  
+}
