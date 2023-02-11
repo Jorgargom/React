575dc90634eb54246165ac5c574fb372d3368a32
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-02-11 16:41:23 +0100

    Jest - Expect - toBe

--- 2023/React-FH/02-counter-app-vite/src/tests/demo.test.js
+++ 2023/React-FH/02-counter-app-vite/src/tests/demo.test.js
@@ -1,7 +1,25 @@
-test('Esta prueba no debe fallar', () => { 
-    
-    if (1 === 0 ) {
-        throw new Error("No puede dividir entre cero");
-    }
 
-})
+
+describe('Test en <demo.test>', () => { 
+    
+    test('Esta prueba no debe fallar', () => { 
+    
+        // 1. Iniciacilización
+    
+        const message1 = "Hello world";
+    
+    
+        // 2. Estímulo
+    
+        const message2 = message1.trim();
+    
+    
+        // 3. Observar el comportamiento esperado
+    
+        expect ( message1 ).toBe( message2 );
+    
+    
+    })
+    
+ })
+
