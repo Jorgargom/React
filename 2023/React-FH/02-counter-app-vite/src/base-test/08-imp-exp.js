737c239e1ea637a3752ab89c8849ba3e5c916e63
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-02-13 22:32:24 +0100

    test 08-imp-exp.js

--- 2023/React-FH/02-counter-app-vite/src/base-test/08-imp-exp.js
+++ 2023/React-FH/02-counter-app-vite/src/base-test/08-imp-exp.js
@@ -4,6 +4,3 @@ import heroes from '../data/heroes';
 export const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
 
 export const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
-
-
-
