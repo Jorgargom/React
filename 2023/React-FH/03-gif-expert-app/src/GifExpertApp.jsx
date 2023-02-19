526c8db8070591f7bc91fd1c0151b1f35a2fdf5c
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-02-19 20:27:33 +0100

    Styles Gif Expert App

--- 2023/React-FH/03-gif-expert-app/src/GifExpertApp.jsx
+++ 2023/React-FH/03-gif-expert-app/src/GifExpertApp.jsx
@@ -2,7 +2,16 @@
 export const GifExpertApp = () => {
   return (
     <>
-    <h1>GifExpertApp</h1>
+    {/* Title */}
+    <h1>GifExpertApp g016SpOBjprAYcRbT5vkWMPo8uyeJ1RK</h1>
+    
+    {/* Input */}
+
+
+    {/* Listado de Gif */}
+      {/* Gif Item */}
+
+    
     </>
   )
 }
