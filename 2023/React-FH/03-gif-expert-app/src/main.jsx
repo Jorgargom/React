87d560b330405d9dd1a2571d977fc5edeccdd290
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-02-19 20:16:39 +0100

    Giff Expert APP started

--- 2023/React-FH/03-gif-expert-app/src/main.jsx
+++ 2023/React-FH/03-gif-expert-app/src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { GifExpertApp } from './GifExpertApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GifExpertApp />
   </React.StrictMode>,
 )
