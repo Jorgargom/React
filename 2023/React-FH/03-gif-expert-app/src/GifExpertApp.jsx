476c3031953fb8ec81a3b71bb50afe33653e13e3
Jorge García Gómez <jorge@jorgegarciagomez.es>
2023-02-20 21:07:41 +0100

    update GifExpertApp

--- 2023/React-FH/03-gif-expert-app/src/GifExpertApp.jsx
+++ 2023/React-FH/03-gif-expert-app/src/GifExpertApp.jsx
@@ -3,9 +3,9 @@ import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
   
-  const [categories, setCategories] = useState(['One Punch' ,'Dragon Ball']);
+  const [categories, setCategories] = useState(['One Punch']);
   
-  const onAddCategory = () => {
+  const onAddCategory = ( newCategory ) => {
     if ( categories.includes(newCategory) ) return;
         setCategories([ newCategory, ...categories ]);
   };
